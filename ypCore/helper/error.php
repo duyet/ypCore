abee15fd821fe37e5088d706e9e54b578356bbc6
LvDuit <duyet2000@gmail.com>
2014-02-11 18:16:20 +0700

    Add function noticePage()

--- ypCore/helper/error.php
+++ ypCore/helper/error.php
@@ -35,6 +35,7 @@ function errorHandler($errNo, $errMess, $errFile, $errLine) {
 	ypLog('[' . date('h:i:s') . "] #$errKey :: ($errNo) :: $error :: $errMess [$errFile ($errLine)]");
 
 ?>
+<!doctype html>
 <html>
 	<head>
 		<title>Oop! Error!</title>
@@ -85,4 +86,60 @@ function errorHandler($errNo, $errMess, $errFile, $errLine) {
 </html>
 <?php
 exit();
+}
+
+
+function noticePage($msg = '', $title = 'Notice page!!', $exit = TRUE) {
+
+?>
+<!doctype html>
+<html>
+	<head>
+		<title><?php echo $title; ?></title>
+
+		<!-- Css -->
+		<style>
+			.clearfix { *zoom: 1;}
+			.clearfix:before, .clearfix:after {display: table; line-height: 0; content: ""}
+			html { font-size: 100%; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;}
+			body { margin: 0; font-family: "Segoe UI", "Segoe WP", "Helvetica Neue", sans-serif; font-size: 14px; line-height: 25px; color: #333333; background-color: #ffffff; }
+			.container { width: 940px; }
+			.container { margin-top:70px; margin-right: auto; margin-left: auto; *zoom: 1; }
+			.container:before,
+			.container:after { display: table; content: ""; line-height: 0; }
+			.container:after { clear: both; }
+			[class*="span"] { float: left; min-height: 1px; margin-left: 20px; }
+			.row { margin-left: -20px; *zoom: 1; }
+			.row:before, .row:after { display: table; content: ""; line-height: 0; }
+			.row:after { clear: both; }
+			h1,h2,h3,h4,h5,h6 { margin: 12.5px 0; font-family: "Segoe UI Light", "Helvetica Neue", "Segoe UI", "Segoe WP", sans-serif; font-weight: 100; line-height: 25px; color: inherit; text-rendering: optimizelegibility; }
+			h1,h2,h3 { line-height: 50px;}
+			h1 { font-size: 60px;}
+			.hero-unit { padding: 60px; margin-bottom: 30px; font-size: 18px; font-weight: 200; line-height: 37.5px; color: inherit; background-color: #eeeeee; -webkit-border-radius: 6px; -moz-border-radius: 6px; border-radius: 6px; }
+			.hero-unit h1 { margin-bottom: 0; font-size: 60px; line-height: 1; color: inherit; letter-spacing: -1px; }
+			.hero-unit li { line-height: 37.5px; }
+			blockquote { border: 0; page-break-inside: avoid; }
+			blockquote { padding: 0 0 0 15px; margin: 0 0 25px; border-left: 5px solid #eeeeee;}
+			blockquote p {  margin-bottom: 0;  font-size: 16px;  font-weight: 300;  line-height: 31.25px;}
+			blockquote small {  display: block;  line-height: 25px;  color: #999999;}
+			blockquote small:before {  content: '\2014 \00A0';} 
+			blockquote {  border-color: #ddd;  border-color: rgba(0, 0, 0, 0.15);}hr {  margin: 25px 0;  border: 0;  border-top: 1px solid #eeeeee;  border-bottom: 1px solid #ffffff;}
+		</style>
+		<!-- / Css -->
+
+		<!-- Error -->
+		
+	</head>
+	<body>
+		<div class="container">
+			<blockquote>
+				<br />
+				<h1>Notice: <?php echo $msg ?></h1> <br />
+				<br />
+			</blockquote>
+		</div>
+	</body>
+</html>
+<?php 
+if ($exit) exit(-1);
 }
