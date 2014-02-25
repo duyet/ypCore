3cfc442e434de3d6b7a55d049b4cd17e619ff517
LvDuit <duyet2000@gmail.com>
2014-02-25 07:41:05 +0700

    Upgrade user class

--- apps/class/user.php
+++ apps/class/user.php
@@ -3,15 +3,15 @@ class __USER {
 	private $_data = array();
 	protected $_registry;
 	const COOKIE_MAX_TIME = 31536; // 365 days * 24 hours * 3600 seconds
-	
+
 	public function __construct(&$registry) {
 		$this->_registry = $registry;
-		
+
 		$this->set('isLogin', FALSE);
 		$this->set('isAdmin', FALSE);
 
 		$this->set('userHashKey', '');
-			
+
 		if ($this->_isLogin()) {
 			$this->set('isLogin', TRUE);
 			$this->set('user_info', $this->_getUserInfo());
@@ -19,15 +19,15 @@ class __USER {
 			$this->set('userHashKey', $this->_getUserHashKey());
 		}
 	}
-	
+
 	public function __set($name, $value) {
 		return $this->_registry->set($name, $value);
 	}
-	
+
 	public function __get($name) {
 		return $this->_registry->get($name);
 	}
-	
+
 	private function _isLogin() {
 		$username = $this->Session->get('username');
 		$time_login = $this->Session->get('time_login');
@@ -75,11 +75,11 @@ class __USER {
 
 		return FALSE;
 	}
-	
+
 	public function isLogin() {
 		return (boolean) $this->get('isLogin');
 	}
-	
+
 	private function _isAdmin() {
 		if (!$this->isLogin()) {
 			return FALSE;
@@ -119,7 +119,7 @@ class __USER {
 	public function isAdmin() {
 		return (boolean) $this->get('isAdmin');
 	}
-	
+
 	public function isLoginToAdminPanel() {
 		// code this later
 		//return $this->Session->get('isAdmin');
@@ -128,10 +128,10 @@ class __USER {
 	private function _getUserInfo() {
 		$userInfo = $this->getUserFromID($this->get('userid'));
 		unset($userInfo['password']);
-		
+
 		return $userInfo;
 	}
-	
+
 	public function getUserInfo() {
 		if (!isset($this->_data['user_info'])) {
 			return FALSE;
@@ -139,12 +139,12 @@ class __USER {
 
 		return $this->_data['user_info'];
 	}
-	
+
 	public function get($name) {
 		if (!isset($this->_data["$name"])) {
 			return FALSE;
 		}
-		
+
 		return $this->_data["$name"];
 	}
 
@@ -152,66 +152,66 @@ class __USER {
 		if (!isset($this->_data['user_info']["$name"])) {
 			return FALSE;
 		}
-		
+
 		return $this->_data['user_info']["$name"];
 	}
 
 	public function set($name, $value) {
 		return $this->_data[$name] = $value;
 	}
-	
+
 	public function setData($data) {
 		if (!is_array($data)) {
 			return FALSE;
 		}
 		$this->_data = array_merge($this->_data, $data);
 	}
-	
+
 	public function getData() {
 		return $this->_data;
 	}
-	
+
 	public function getUserFromID($user_id) {
 		$user_id = intval($user_id);
-		$this->Db->query("SELECT * FROM `yp_users` 
+		$this->Db->query("SELECT * FROM `yp_users`
 		WHERE `userid` = ". $user_id ."");
-		
+
 		if ($this->Db->num_rows() == 0) {
 			return FALSE;
 		}
-		
+
 		return $this->Db->fetch();
 	}
-	
+
 	public function getUserFromUsername($username) {
 		$username = htmlspecialchars((string) $username);
 		if (empty($username)) {
 			return FALSE;
 		}
-		$this->Db->query("SELECT * FROM `yp_users` 
+		$this->Db->query("SELECT * FROM `yp_users`
 		WHERE `username` = '". $this->Db->e($username) ."'");
-		
+
 		if ($this->Db->num_rows() == 0) {
 			return FALSE;
 		}
-		
+
 		return $this->Db->fetch();
 	}
-	
+
 	public function getUserFromEmail($email) {
 		$email = (string) $email;
 		$this->_registry->get('Loader')->helper('string');
 		if (!isEmail($email)) {
 			return FALSE;
 		}
-		
-		$this->Db->query("SELECT * FROM `yp_users` 
+
+		$this->Db->query("SELECT * FROM `yp_users`
 		WHERE `username` = '". $this->Db->e($email) ."'");
-		
+
 		if ($this->Db->num_rows() == 0) {
 			return FALSE;
 		}
-		
+
 		return $this->Db->fetch();
 	}
 }
--- apps/controller/Login/Index.php
+++ apps/controller/Login/Index.php
@@ -3,17 +3,24 @@ class Controller_Login_Index extends ypController {
 	public function Index() {
 		$isAdminLogin = $this->Request->REQUEST['admin_login'] ? TRUE : FALSE;
 		$this->Loader->controller('User/User');
-		if ($this->User->isLogin()) {
+		
+		$isAdminReLogin = FALSE;
+		if ($this->User->isLogin() AND !$this->User->isAdmin() AND $isAdminLogin) {
+			// Re-login
+			$isAdminReLogin = TRUE;
+		}
+		else if ($this->User->isLogin()) {
 			if ($this->Request->REQUEST['redirect_to']) {
 				$this->redirect($this->Request->REQUEST['redirect_to']);	
 			}
 			$this->redirect($this->Link->build('User/Account', TRUE));
 		}
 
-		$this->set('username', '');
-		if ($this->Request->GET['username']) {
-			$this->set('username', $this->Request->GET['username']);
+		$username = '';
+		if ($this->Request->GET['username'] AND $isAdminReLogin == TRUE) {
+			$username = $this->Request->GET['username'];
 		}
+		$this->set('username', $username);
 
 		$redirect_to = '';
 		if($this->Request->GET['redirect_to']) {
@@ -26,7 +33,7 @@ class Controller_Login_Index extends ypController {
 
 		$this->loadLanguage('Login/Form');
 		$this->set('redirect_to', $redirect_to);
-		if (!$this->Request->GET['is_redirect']) {
+		if ($this->Request->GET['is_redirect'] != '1') {
 			$this->Link->addParam('hl', $this->Setting->get('language'));
 			$this->Link->addParam('is_redirect', '1');
 			$this->Link->addParam('is_human', '1');
@@ -39,7 +46,12 @@ class Controller_Login_Index extends ypController {
 
 		$this->set('form_action', $this->Link->build('Login/Progress'));
 		$this->set('text_register_link', sprintf($this->_data['text_register_link'], $this->Link->build('Register',TRUE)));
-		$this->set('admin_login', $isAdminLogin);		
+		$this->set('admin_login', $isAdminLogin);
+		$this->set('hl', $this->Request->REQUEST['hl']);
+		$this->set('is_redirect', $this->Request->REQUEST['is_redirect']);
+		$this->set('is_human', $this->Request->REQUEST['is_human']);
+		$this->set('admin_login', $this->Request->REQUEST['admin_login']);		
+		$this->set('register_link', $this->Link->build('Register', FALSE, array('redirect_to' => $redirect_to)));
 
 		$this->setTemplate('module/Login/Form.php');
 		$this->setTemplateChild(array(
