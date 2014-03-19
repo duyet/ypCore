a7a1f982ee0d5e7012a1adff35555bfb7e045577
LvDuit <duyet2000@gmail.com>
2014-03-19 19:56:31 +0700

    Change to jQuery local if not connect to network at header

--- apps/admin/view/simple/layout/Footer.php
+++ apps/admin/view/simple/layout/Footer.php
@@ -1,7 +1,9 @@
+<script>window.jQuery || document.write('<scr' + 'ipt src="{$static_url}/javascript/jquery/jquery-1.9.1.js"><\/scri' + 'pt>')</script>
 
-<script type="text/javascript" src="{$static_url}/javascript/bootstrap-dropdown.js"></script>
-<script type="text/javascript" src="{$static_url}/javascript/jquery/jquery-1.9.1.js"></script>
 <script type="text/javascript" src="{$template_url}/js/main.js"></script>
-
+<script type="text/javascript" src="{$static_url}/javascript/bootstrap/bootstrap-dropdown.js"></script>
+<script>
+	$('.dropdown').dropdown()
+</script>
 </body>
 </html>
